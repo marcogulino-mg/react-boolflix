28de35dc33fb30ddb9fccdbb42608eff55fce37f
marco-gulino <marcogulino98@gmail.com>
2025-02-27 18:52:48 +0100

    [feat] create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 // Import Contexts
 import MediaContext from "./contexts/MediaContext";
-
+// Import Pages
+import Homepage from "./pages/HomePage";
+import Mediapage from "./pages/MediaPage";
 // import "./App.css";
 
 function App() {
-  const [media, setMedia] = useState([]);
-  return <></>;
+  // State Vars
+  const [medias, setMedias] = useState([]);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index Component={Homepage} />
+          <Route path="search" Component={Mediapage} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/MediaPage.jsx
@@ -0,0 +1,6 @@
+// Import MediaList.jsx
+import MediaList from "../components/MediaList";
+
+export default function Mediapage() {
+  return <MediaList />;
+}
